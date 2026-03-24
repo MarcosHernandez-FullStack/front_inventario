786a846590237231cf71efcc45fcb4f0774c7bef
Marcos Hernández Barba <marcoshernandez171711@gmail.com>
2026-03-24 00:23:14 -0500

    Actualizacion del environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:     'https://tu-api.azurewebsites.net/api',
-  signalrUrl: 'https://tu-api.azurewebsites.net/hubs/notificaciones',
+  apiUrl:     'https://back-inventario-app.blackhill-7330c334.eastus2.azurecontainerapps.io/api',
+  signalrUrl: 'https://back-inventario-app.blackhill-7330c334.eastus2.azurecontainerapps.io/hubs/notificaciones',
 };
